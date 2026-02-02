68a33756519555fe5bc2af584a32628d429484ea
Csendes Gábor <csendes@valahol>
2026-02-02 15:59:57 +0100

    Refaktorálva a dobás

--- src/Poker.java
+++ src/Poker.java
@@ -2,10 +2,10 @@ import java.util.Random;
 
 public class Poker {
     public static void play() {
-        machineThrow();
-        humanThrow();
+        throwDice("Gép");
+        throwDice("Ember");
     }
-    public static void machineThrow() {
+    private static void throwDice(String role) {
         Random random = new Random();
         int kocka1 = random.nextInt(6) + 1;
         int kocka2 = random.nextInt(6) + 1;
@@ -14,20 +14,7 @@ public class Poker {
         int kocka5 = random.nextInt(6) + 1;
         System.out.printf(
             "%6s: %d %d %d %d %d\n",
-            "Gép",
-            kocka1, kocka2, kocka3, kocka4, kocka5
-        );
-    }
-    public static void humanThrow() {
-        Random random = new Random();
-        int kocka1 = random.nextInt(6) + 1;
-        int kocka2 = random.nextInt(6) + 1;
-        int kocka3 = random.nextInt(6) + 1;
-        int kocka4 = random.nextInt(6) + 1;
-        int kocka5 = random.nextInt(6) + 1;
-        System.out.printf(
-            "%6s: %d %d %d %d %d\n",
-            "Ember",
+            role,
             kocka1, kocka2, kocka3, kocka4, kocka5
         );
     }
